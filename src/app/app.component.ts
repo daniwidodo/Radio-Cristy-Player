9d9a6a874609e159fb25e42f11a2be4330db3ba1
daniwidodo <dani.widodo87@gmail.com>
2018-03-21 14:27:16 +0800

    admob bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+//import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -15,9 +15,10 @@ export class MyApp {
     private platform: Platform, 
     statusBar: StatusBar, 
     splashScreen: SplashScreen,
-    private adMobFree: AdMobFree,
+//    private adMobFree: AdMobFree,
   ) {
     platform.ready().then(() => {
+      /*
       // Admob Free
       let admobid;
         if(this.platform.is('android')) {
@@ -31,15 +32,18 @@ export class MyApp {
             interstitial: 'ca-app-pub-3473119910769766/1868919368'
           };
         };
+        
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.showInterstitialAd();
+      */
       statusBar.styleBlackTranslucent();
       splashScreen.hide();
 
     });
   }
 
+  /*
   async showInterstitialAd() {
     try {
       const interstitialConfig: AdMobFreeInterstitialConfig = {
@@ -57,6 +61,7 @@ export class MyApp {
       console.error(e)
     }
   }
+  */
   
 }
 
